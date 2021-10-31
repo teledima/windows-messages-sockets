0840962a38d739c02b74a8ab058c4c4a8c8a24dc
teledima <teledima00@gmail.com>
2021-10-31 19:04:19 +0500

    Add mapping games.db and empty helper class

---
+++ HelperSockets/SourceGames.cs
@@ -0,0 +1,18 @@
+﻿namespace HelperSockets
+{
+    public class SourceGames
+    {
+        public int GamesId { get; set; }
+        public string GamesName { get; set; }
+        public int GamesCategoryGameId { get; set; }
+        public int GamesCategoryCategoryId { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public int DownloadableContentsId { get; set; }
+        public string DownloadableContentsName { get; set; }
+        public int DownloadableContentsGameId { get; set; }
+        public int AchievementId { get; set; }
+        public string AchievementName { get; set; }
+        public int AchievementGameId { get; set; }
+    }
+}
---
+++ HelperSockets/SourceGamesHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace HelperSockets
+{
+    public static class SourceGamesHelper
+    {
+        public static List<SourceGames> GetSource()
+        {
+
+            return new List<SourceGames>();
+        }
+    }
+}
